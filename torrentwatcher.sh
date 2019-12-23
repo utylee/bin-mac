0a9d5eb7be5188147aec8ed1cbab5bd8bade4064
utylee <utylee@gmail.com>
2019-12-23 23:44:23 +0900

    torrentwatcher.sh chg to new macos bin/bash to /usr/local/bin/bash

--- torrentwatcher.sh
+++ torrentwatcher.sh
@@ -1,3 +1,4 @@
-#!/bin/bash
-#/usr/local/bin/python3 /Users/utylee/bin/torrentwatcher-for-osx.py
-/Users/utylee/.pyenv/versions/3.6.2/bin/python3 /Users/utylee/bin/torrentwatcher-for-osx.py
+#!/usr/local/bin/bash
+# macos 신버전에서 bash에서 zsh로 기본 쉘이 바뀜에 따라 /bin/bash로는 안돼서 이렇게 하니 됩니다
+#open /Applications/Firefox.app
+/Users/utylee/.pyenv/versions/3.6.8/bin/python3 /Users/utylee/bin/torrentwatcher-for-osx.py
