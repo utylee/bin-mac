27030067ccf4a2369b60258d2f6ed57e5ec7183f
utylee <utylee@gmail.com>
2021-11-27 00:52:47 +0900

    loginscript.plist, torrentwatcher.sh: chg ip to 1     domain things

--- torrentwatcher.sh
+++ torrentwatcher.sh
@@ -1,4 +1,5 @@
 #!/usr/local/bin/bash
 # macos 신버전에서 bash에서 zsh로 기본 쉘이 바뀜에 따라 /bin/bash로는 안돼서 이렇게 하니 됩니다
 #open /Applications/Firefox.app
-/Users/utylee/.pyenv/versions/3.6.8/bin/python3 /Users/utylee/bin/torrentwatcher-for-osx.py
+#/Users/utylee/.pyenv/versions/3.6.8/bin/python3 /Users/utylee/bin/torrentwatcher-for-osx.py
+/Users/utylee/.pyenv/versions/3.8.12/bin/python3 /Users/utylee/bin/torrentwatcher-for-osx.py
